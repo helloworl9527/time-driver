83fd4961c9cc6eab8a8e84b56b2af4575f13a552
helloworl9527 <128283395+helloworl9527@users.noreply.github.com>
2024-10-05 18:43:18 +0800

    Update main.py
    更改info。json路径

--- driver/main.py
+++ driver/main.py
@@ -11,8 +11,14 @@ def main():
  
         success_file.write(f"程序运行成功时间: {success_time}\n")
         
-    with open('info.json', 'r') as f:
-        data = json.load(f)
+    # 构建 info.json 文件的相对路径（上一层文件夹）
+    info_json_path = os.path.join('..', 'info.json')
+    
+    # 读取 info.json 文件
+    with open(info_json_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    # with open('info.json', 'r') as f:
+    #     data = json.load(f)
 
     for item in data:
         # print(item['license_id'], item['region_id'], item['price_per_month'], item['minimum_stay_month'], item['desc'])
